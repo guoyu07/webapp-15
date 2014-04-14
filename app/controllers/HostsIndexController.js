1a4a2a686f88e33f4c9caede31d1b77991e2ad22
gzurbach <guillaume.zurbach@gmail.com>
2014-04-13 22:47:29 -0700

    Reorganize hosts route/controller

--- app/controllers/HostsIndexController.js
+++ app/controllers/HostsIndexController.js
@@ -4,12 +4,44 @@
 
 App.HostsIndexController = Ember.ArrayController.extend({
 
-    needs: ['hosts'],
+    searchTerm: null,
+    departmentFilter: null,
+    departmentFilterOptionsBinding: 'departments',
 
-    contentBinding: 'controllers.hosts.arrangedContent',
+    departments: function () {
+        return this.store.find('department');
+    }.property(),
+
+    parameters: function () {
+        return {
+            'searchTerm': $.trim(this.get('searchTerm')) || null,
+            'dpt': this.get('departmentFilter.id')
+        };
+    }.property('searchTerm', 'departmentFilter.id'),
 
     totalHosts: function () {
         return this.store.metadataFor('host').total;
-    }.property('content', 'content.@each', 'content.length')
+    }.property('content', 'content.@each', 'content.length'),
 
+    actions: {
+        searchHosts: function () {
+            var self = this;
+            this.store
+                .find('host', this.get('parameters'))
+                .then(function (hosts) {
+                    self.set('content', hosts);
+                });
+        },
+        loadMoreHosts: function () {
+            // Initialize variables
+            var newOffset = this.store.metadataFor('host').offset + 20,
+                params = Ember.$.extend(true, this.get('parameters') || {}, {offset: newOffset});
+
+            // Find next page of content and update
+            this.store.find('host', params).then(function (hosts) {
+                this.get('content').addObjects(hosts.get('content'));
+            });
+
+        }
+    }
 });
--- app/routes/HostsIndexRoute.js
+++ app/routes/HostsIndexRoute.js
@@ -3,5 +3,11 @@
  */
 
 App.HostsIndexRoute = Ember.Route.extend({
-
+    setupController: function (controller) {
+        this.store
+            .find('host', controller.get('parameters'))
+            .then(function (hosts) {
+                controller.set('content', hosts);
+            });
+    }
 });
