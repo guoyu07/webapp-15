a012860424271e62b57b0839b59217d0e7397b46
gzurbach <guillaume.zurbach@gmail.com>
2014-07-20 14:23:57 -0700

    Only retrieve memberships if user is authenticated

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,7 +5,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function () {
-        return this.store.find('membership', { userId: this.controllerFor('application').get('currentUser.id') });
+        if (this.controllerFor('application').get('isAuthenticated')) {
+            return this.store.find('membership', { userId: this.controllerFor('application').get('currentUser.id') });
+        }
     },
     setupController: function (controller, model) {
         this.controllerFor('memberships').set('content', model);
