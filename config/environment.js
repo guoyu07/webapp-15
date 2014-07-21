e7959375d7f2c4157d6731d5e6f1fb7bf1c9cb8a
gzurbach <guillaume.zurbach@gmail.com>
2014-07-20 21:12:21 -0700

    Always provide a SERVER_BASE_URL otherwise login break in production

--- config/environment.js
+++ config/environment.js
@@ -5,6 +5,7 @@ module.exports = function (environment) {
         environment: environment,
         locationType: 'hash',
         baseUrl: '/',
+        SERVER_BASE_URL: '',
         EmberENV: {
             FEATURES: {
                 // Here you can enable experimental features on an ember canary build
