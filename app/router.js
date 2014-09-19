c7ef6cb093d572ef785a2b437d73d16d40447511
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-09-19 16:33:14 -0700

    Update router

--- app/router.js
+++ app/router.js
@@ -5,31 +5,31 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function () {
-    this.resource('hosts', { path: '/hosts' }, function () {
-        this.route("new");
-        this.resource("host", { path: "/:host_id" }, function () {
-            this.route("edit");
-        });
-    });
-    this.resource('wwoofers', { path: '/wwoofers' }, function () {
-        this.route("new");
-        this.resource("wwoofer", { path: "/:wwoofer_id" }, function () {
-            this.route("edit");
-            this.route('membership');
-        });
-    });
     this.route('login');
     this.route('reset-password');
-    this.resource('users', { path: '/users' }, function () {
+    this.resource('hosts', function () {
         this.route("new");
-        this.resource("user", { path: "/:user_id" }, function () {
-            this.route("edit");
-        });
     });
-    this.resource('memberships', { path: '/memberships' }, function () {
+    this.resource("host", { path: "/host/:host_id" }, function () {
+        this.route("edit");
+    });
+    this.resource('wwoofers', function () {
+        this.route("new");
+    });
+    this.resource("wwoofer", { path: "/wwoofer/:wwoofer_id" }, function () {
+        this.route("edit");
+        this.route('membership');
+    });
+    this.resource('users', function () {
+        this.route("new");
+    });
+    this.resource("user", { path: "/user/:user_id" }, function () {
+        this.route("edit");
+    });
+    this.resource('memberships', function () {
         this.route('reminders');
     });
-    this.resource('payment', { path: '/payment' }, function () {
+    this.route('payment', function () {
         this.route("complete");
         this.route("cancel");
     });
