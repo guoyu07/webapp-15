f07d436bc3df60ce37f7ad109cb3021d1802048d
James Futhey <futhey@gmail.com>
2014-09-18 19:41:23 -0700

    Fixes issue #15

---
+++ app/helpers/substr.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Handlebars.registerBoundHelper('substr', function(value, options) {
+
+    var opts = options.hash;
+    var start = opts.start || 0;
+    var len = opts.max;
+    var out = value.substr(start, len);
+    if (value.length > len)
+        out += '...';
+
+    return new Ember.Handlebars.SafeString(out);
+});
+
