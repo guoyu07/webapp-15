eef170fdf77f54d7707ef5a5bf84923e4d640547
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-09-18 21:41:27 -0700

    Fix some jshint + remove duplicate helper

--- app/app.js
+++ app/app.js
@@ -12,19 +12,3 @@ var App = Ember.Application.extend({
 loadInitializers(App, 'webapp');
 
 export default App;
-
-
-Ember.Handlebars.registerBoundHelper('substr', function(value, options) {
-
-    var opts = options.hash;
-
-    var start = opts.start || 0;
-    var len = opts.max;
-
-    var out = value.substr(start, len);
-
-    if (value.length > len)
-        out += '...';
-
-    return new Handlebars.SafeString(out);
-});
