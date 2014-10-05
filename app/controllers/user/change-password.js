ee69ad50b779a589b2d4aafc2cd4bff205fdac78
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-10-05 12:33:15 -0700

    Add page to change a user's password

---
+++ app/controllers/user/change-password.js
@@ -0,0 +1,77 @@
+/**
+ * Ember controller for password update.
+ */
+import Ember from 'ember';
+import ValidationsMixin from '../../mixins/validations';
+import Regex from '../../utils/regex';
+
+export default Ember.Controller.extend(ValidationsMixin, {
+
+    needs: ['application'],
+
+    password: null,
+    passwordConfirmation: null,
+    isLoading: false,
+
+    actions: {
+        /**
+         * Updates a user's password.
+         */
+        changePassword: function () {
+
+            if (this.get('isLoading')) {
+                return;
+            }
+
+            // Validate form
+            var self = this;
+            this.validate().then(function() {
+
+                // Get the current user id
+                var currentUserId = self.get('controllers.application.currentUser.id');
+                Ember.assert('User id cannot be null', currentUserId);
+
+                // Set controller in loading state
+                self.set('isLoading', true);
+
+                // Prepare URL
+                var adapter = self.store.adapterFor('application'),
+                    url = [ adapter.get('host'), adapter.get('namespace'), 'users', currentUserId, 'change-password' ].join('/');
+
+                // Update password
+                Ember.$.ajax({
+                    type: 'POST',
+                    url: url,
+                    data: {
+                        newPassword: self.get('password')
+                    }
+                }).done(function () {
+                    // Notify user
+                    alertify.success("Your password has been updated.");
+
+                    // Go to login page
+                    self.transitionToRoute('user.edit', currentUserId);
+                }).fail(function () {
+                    // Notify user
+                    alertify.error("An error occurred.");
+                }).always(function () {
+                    self.set('isLoading', false);
+                });
+            }).catch(function () {
+                alertify.error("Your submission is invalid.");
+            });
+        }
+    },
+
+    validations: {
+        password: {
+            presence: true,
+            length: { minimum: 8, maximum: 25 },
+            confirmation: true
+        },
+        passwordConfirmation: {
+            presence: true,
+            length: { minimum: 8, maximum: 25 }
+        }
+    }
+});
--- app/router.js
+++ app/router.js
@@ -8,30 +8,31 @@ Router.map(function () {
     this.route('login');
     this.route('reset-password');
     this.resource('hosts', function () {
-        this.route("new");
+        this.route('new');
     });
-    this.resource("host", { path: "/host/:host_id" }, function () {
-        this.route("edit");
+    this.resource('host', { path: '/host/:host_id' }, function () {
+        this.route('edit');
     });
     this.resource('wwoofers', function () {
-        this.route("new");
+        this.route('new');
     });
-    this.resource("wwoofer", { path: "/wwoofer/:wwoofer_id" }, function () {
-        this.route("edit");
+    this.resource('wwoofer', { path: '/wwoofer/:wwoofer_id' }, function () {
+        this.route('edit');
         this.route('membership');
     });
     this.resource('users', function () {
-        this.route("new");
+        this.route('new');
     });
-    this.resource("user", { path: "/user/:user_id" }, function () {
-        this.route("edit");
+    this.resource('user', { path: '/user/:user_id' }, function () {
+        this.route('edit');
+        this.route('change-password');
     });
     this.resource('memberships', function () {
         this.route('reminders');
     });
     this.route('payment', function () {
-        this.route("complete");
-        this.route("cancel");
+        this.route('complete');
+        this.route('cancel');
     });
 });
 
