323b9f6c3feb26cefadf570bde5e18ecea0b3cec
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-10-05 18:35:52 -0700

    Fix host creation form

--- app/routes/hosts/new.js
+++ app/routes/hosts/new.js
@@ -5,10 +5,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function () {
+
+        // Get the current user id
+        var userId = this.controllerFor('application').get('currentUser.id');
+        if (!userId) {
+            this.transitionTo('index');
+        }
+
+        // Create a new host record attached to the current logged in user
         var address = this.store.createRecord('address');
-        return this.store.createRecord('host', {
-            farmName: "La Ferme de M. Seguin",
-            address: address
+        var self = this;
+        return this.store.find('user', userId).then(function (user) {
+            return self.store.createRecord('host', {
+                farmName: "La Ferme de M. Seguin",
+                user: user,
+                address: address
+            });
         });
     }
 });
