5d2e89efb521b8d3d51cb4f0ddc942739c41dfc0
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-10-09 22:56:29 -0700

    Updated the activities

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -8,13 +8,13 @@ export default Ember.ArrayController.extend({
      * List of all activities proposed by the hosts.
      */
     allActivities: [
-        'husbandry',
-        'market-gardening',
-        'transformation',
-        'fields',
-        'wood',
-        'picking',
-        'eco-construction',
-        'alternative-technologies'
+        'ranching',
+        'dairy',
+        'vineyard',
+        'orchard',
+        'vegetables',
+        'forestry',
+        'grain-farming',
+        'permaculture'
     ]
 });
