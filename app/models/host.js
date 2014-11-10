6943397b8cb273099d00d470b0e0084d0cdcf618
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-11-09 19:12:06 -0800

    Increment host full description limit to 3000 characters (Fixes #18)

--- app/models/host.js
+++ app/models/host.js
@@ -43,7 +43,7 @@ export default DS.Model.extend(ValidationsMixin, {
         },
         fullDescription: {
             presence: true,
-            length: { minimum: 5, maximum: 1500 }
+            length: { minimum: 5, maximum: 3000 }
         },
         webSite: {
             url: { allowBlank: true }
