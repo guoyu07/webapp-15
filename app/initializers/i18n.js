44655a2102a9da31f3d711b061633284b65f7f7a
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-11-27 13:44:08 -0800

    Fix translation file loading with internet explorer

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default {
     name: 'i18n',
 
-    initialize: function () {
+    initialize: function (container, app) {
 
         // Set default locale for plurals based on browser's language
         Ember.I18n.locale = window.navigator.userLanguage || window.navigator.language;
@@ -13,12 +13,18 @@ export default {
         Ember.TextArea.reopen(Ember.I18n.TranslateableAttributes);
         Ember.Select.reopen(Ember.I18n.TranslateableProperties);
 
+        // Defer loading the application until translation file is loaded
+        app.deferReadiness();
+
         // Get locale file from server
         var request = Ember.$.get('/api/translations');
 
         // Load translations
         request.done(function (data) {
             Ember.I18n.translations = data;
+
+            // Continue to render the application
+            app.advanceReadiness();
         });
 
         request.fail(function () {
