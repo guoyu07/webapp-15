39e4ac1cd228ff4f61fb07cbba6edea94a49ad81
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2014-12-16 15:40:27 +0100

    Force host description length (300 min and 5000 max) (Fixes #87)

--- app/models/host.js
+++ app/models/host.js
@@ -48,7 +48,7 @@ export default DS.Model.extend(ValidationsMixin, {
         },
         fullDescription: {
             presence: true,
-            length: { minimum: 5, maximum: 3000 }
+            length: { minimum: 300, maximum: 5000 }
         },
         webSite: {
             format: {
