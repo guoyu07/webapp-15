4fa097d1219fb9b364b1b41e91f665f9e67721ce
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-01-03 16:21:46 -0800

    Remove unnecessary check in new wwoofer route

--- app/routes/wwoofers/new.js
+++ app/routes/wwoofers/new.js
@@ -7,16 +7,10 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     model: function () {
 
-        // Get the current user id
-        var userId = this.controllerFor('application').get('currentUser.id');
-        if (!userId) {
-            this.transitionTo('index');
-        }
-
         // Create a new wwoofer record attached to the current logged in user
         var address = this.store.createRecord('address');
         var self = this;
-        return this.store.find('user', userId).then(function (user) {
+        return this.store.find('user', this.get('session.user.id')).then(function (user) {
             return self.store.createRecord('wwoofer', {
                 user: user,
                 address: address
