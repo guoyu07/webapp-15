fbd229e4b74f45de8dd946019b9938b95e167dd3
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-01-17 22:48:01 -0800

    Use latitude and longitude from database

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,45 +2,26 @@
  * Ember component for google map.
  */
 import Ember from 'ember';
-import config from '../config/environment';
 
 export default Ember.Component.extend({
 
     /**
-     * Id of the address we want to show the map for.
+     * Coordinates of the address we want to show the map for.
      */
-    addressId: null,
+    latitude: null,
+    longitude: null,
 
     /**
      * URL of the map.
      */
     mapUrl: function () {
-        var coordinates = this.get('coordinates') || { lat: 46, lng: 3 };
+        var latitude = this.get('latitude') || 46;
+        var longitude = this.get('longitude') || 3;
         var url = '//maps.googleapis.com/maps/api/staticmap?' +
-            'center=' + coordinates.lat + ','+ coordinates.lng +
+            'center=' + latitude + ','+ longitude +
             '&zoom=6' +
             '&size=523x300' +
-            '&markers=color:red|' + coordinates.lat + ','+ coordinates.lng;
+            '&markers=color:red|' + latitude + ',' + longitude;
         return encodeURI(url);
-    }.property('coordinates'),
-
-    /**
-     * Gets the coordinates of the address.
-     */
-    coordinates: function () {
-
-        if (!this.get('addressId')) {
-            return;
-        }
-
-        // Prepare URL
-        var url = [ config.apiHost, config.apiNamespace, 'addresses', this.get('addressId'), 'get-coordinates' ].join('/');
-
-        // Get the coordinates of the map
-        var get = Ember.$.get(url);
-        var self = this;
-        get.done(function (data) {
-            self.set('coordinates', data.coordinates);
-        });
-    }.property('addressId')
+    }.property('latitude', 'longitude')
 });
