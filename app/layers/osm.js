7628545a6add4d4cd54f51504be6fa1700308fe7
Maxime Viry <chinow42@gmail.com>
2015-02-04 20:10:22 -0800

    forgot some files

--- app/layers/osm.js
+++ app/layers/osm.js
@@ -1,3 +1,11 @@
 /**
  * Created by MaximeV on 2/4/2015.
  */
+import Ember from 'ember';
+
+
+App.OSMLayer = Ember.Object.extend(EmberLeaflet.LayerMixin,{
+    _newLayer : function() {
+        return new L.tileLayer.provider('MapQuestOpen');
+    }
+});
--- app/views/hosts/map.js
+++ app/views/hosts/map.js
@@ -3,39 +3,16 @@
  */
 import App from '../../app';
 import Ember from 'ember';
-
-App.HostsLayer =  Ember.Object.extend(EmberLeaflet.LayerMixin,{
-    geoJsonLayer : null,
-    markers : null,
-    _newLayer : function () {
-        this.geoJsonLayer = new L.geoJson();
-        this.markers = L.markerClusterGroup({ disableClusteringAtZoom: 9 });
-        var dataRequest = Ember.$.get("/api/host-coordinates?limit=2000");
-        var self = this;
-        dataRequest.done(function (data) {
-            self.geoJsonLayer.addData(data);
-            self.markers.addLayer(self.geoJsonLayer);
-        });
-        return this.markers;
-    },
-    requestFeatures(params) {
-
-    }
-});
-
-App.OSMLayer = Ember.Object.extend(EmberLeaflet.LayerMixin,{
-    _newLayer : function() {
-        return new L.tileLayer.provider('MapQuestOpen');
-    }
-});
+import HostsLayer from '../../layers/hosts';
+import OSMLayer from '../../layers/osm';
 
 export default EmberLeaflet.MapView.extend({
     zoom : 6,
     center : L.latLng(46.45, 2.61),
     options : {maxZoom : 12},
     childLayers : [
-        App.OSMLayer,
-        App.HostsLayer
+        OSMLayer,
+        HostsLayer
     ]
 });
 
