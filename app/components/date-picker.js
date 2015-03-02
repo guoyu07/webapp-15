e9e6f13ce77e373bb34a201c30defe6768691cdd
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-03-01 17:36:57 -0800

    Convert date picker (#118)

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -1,33 +1,57 @@
 /**
- * Ember component for datetime picker.
+ * Ember component for date picker.
  */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    dateString: null,
 
-    didInsertElement: function () {
-        var self = this;
-        var dp = Ember.$('#datetimepicker');
+    selectedDay: null,
+    selectedMonth: null,
+    selectedYear: null,
 
-        // Initializes picker
-        dp.datetimepicker({
-            pickTime: false,
-            viewMode: 'years',
-            maxDate: this.get('maxDate'),
-            language: this.get('locale')
-        }).on('dp.change', function () {
-            self.set('value', dp.data('DateTimePicker').getDate()._d);
-        });
+    days: function () {
+        var days = [];
+        var endOfMonth = this.daysInMonth(this.get('selectedMonth.value'), this.get('selectedYear'));
+        for (var i = 1; i <= endOfMonth; i++) { days.push(i); }
+        return days;
+    }.property('selectedMonth.value', 'selectedYear'),
 
-        // Disable picker
-        if (this.get('disabled')) {
-            dp.data('DateTimePicker').disable();
+    months: function () {
+        var months = [];
+        for (var i = 0; i <= 11; i++) {
+            months.push({ value: i, name: moment().months(i).format("MMMM") });
+        }
+        return months;
+    }.property(),
+
+    years: function () {
+        var years = [];
+        for (var i = moment().subtract(18, 'years').year(); i >= 1900; i--) { years.push(i); }
+        return years;
+    }.property(),
+
+    daysInMonth: function (month, year) {
+        return moment({ month: month, year: year }).endOf('month').date();
+    },
+
+    selectedDate: function (key, value) {
+
+        // Set date
+        if (arguments.length > 1 && value) {
+            this.set('selectedDay', value.date());
+            this.set('selectedMonth.value', value.month());
+            this.set('selectedYear', value.year());
         }
 
-        // Set initial value on first load
-        if (this.get('value')) {
-            dp.data('DateTimePicker').setDate(this.get('value'));
-        }
-    }
+        // Get date
+        var day = this.get('selectedDay');
+        var month = this.get('selectedMonth.value');
+        var year = this.get('selectedYear');
+        if (day === null || month === null || year === null) { return moment(); }
+
+        var daysInMonth = this.daysInMonth(month, year);
+        day = (day > daysInMonth) ? daysInMonth : day;
+
+        return moment({ day: day, month: month, year: year });
+    }.property('selectedDay', 'selectedMonth.value', 'selectedYear')
 });
