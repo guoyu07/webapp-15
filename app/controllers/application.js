610bdb63e43ff1ddfe130b85c1a5bf6f9abcd014
chinow42 <chinow42@gmail.com>
2015-03-12 21:43:55 -0700

    finish container-fluid transformation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -18,9 +18,12 @@ export default Ember.Controller.extend(ValidationsMixin, {
     impersonatedUserEmail: null,
 
     wooferProfileClass: function () {
-        return this.get('userMemberships.hasHostMemberships') ?  'glyphicon glyphicon-ok' : 'glyphicon glyphicon-warning-sign';
+        return this.get('userMemberships.hasWwoofMemberships') ?  'glyphicon glyphicon-ok' : 'glyphicon glyphicon-warning-sign';
     }.property(),
 
+    hostProfileClass: function () {
+        return this.get('userMemberships.hasHostMemberships') ?  'glyphicon glyphicon-ok' : 'glyphicon glyphicon-warning-sign';
+    }.property(),
     actions: {
         impersonateUser: function () {
 
