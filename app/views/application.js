9cbfdd17eaec60fd153b8a50a71a8e28d3e8feab
chinow42 <chinow42@gmail.com>
2015-03-15 14:47:08 -0700

    change $ to Ember.$

--- app/views/application.js
+++ app/views/application.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
+    import Ember from 'ember';
 
 export default Ember.View.extend({
     didInsertElement: function () {
-        $('.navbar-collapse').click('a:not(.dropdown-toggle)', function(e) {
+        Ember.$('.navbar-collapse').click('a:not(.dropdown-toggle)', function(e) {
             if(Ember.$(e.target).hasClass('dropdown-toggle')) {
                 return;
             }
-            $('.navbar-collapse').collapse('hide');
+            Ember.$('.navbar-collapse').collapse('hide');
         });
     }
 });
