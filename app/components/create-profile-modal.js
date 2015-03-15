26adcbc1dbd7e9babc256bbd454fa20746418110
chinow42 <chinow42@gmail.com>
2015-03-14 17:50:34 -0700

    Fix profile undefined and not fulfilled

--- app/components/create-profile-modal.js
+++ app/components/create-profile-modal.js
@@ -1,4 +1,4 @@
-/**
+/**observes
  * Ember component for create profile modal
  */
 import Ember from 'ember';
@@ -7,13 +7,19 @@ export default Ember.Component.extend({
     hasBeendisplayed : false,
 
     displayed: function () {
+
+        if (this.get('session.user.wwoofer.isFulfilled') === false)
+            return;
+
+        if (this.get('session.user.host.isFulfilled') === false)
+            return;
+
         if (this.get('session.user.wwoofer.id') == null && this.get('session.user.host.id') == null && this.get('hasBeendisplayed') === false)
         {
             $('#createprofileModal').modal('show');
             this.set('hasBeendisplayed', true);
-            return true;
         }
-    }.observes('session.user.wwoofer.id', 'session.user.host.id'),
+    }.observes('session.user.wwoofer.isFulfilled','session.user.wwoofer.id', 'session.user.host.isFulfilled', 'session.user.host.id'),
 
     actions : {
         close: function () {
