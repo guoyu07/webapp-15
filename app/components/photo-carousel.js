7be34a5013fc98331070b7d623b66414e2c277bd
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-04-18 14:56:32 -0700

    Remove Swipeshow, switch to Bootstrap carousel

---
+++ app/components/photo-carousel.js
@@ -0,0 +1,25 @@
+/**
+ * Ember component to display a bootstrap carousel.
+ */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: 'div',
+    classNames: ['carousel', 'slide'],
+    attributeBindings: ['dataRide:data-ride'],
+    dataRide: 'carousel',
+    didInsertElement: function () {
+
+        // Set active class on first photo
+        Ember.$('.carousel-inner div.item').first().addClass('active');
+        Ember.$('.carousel-indicators li').first().addClass('active');
+
+        // Add values to data-slide-to items
+        Ember.$('.carousel-indicators li').each(function (index, li) {
+            Ember.$(li).attr('data-slide-to', index);
+        });
+
+        // Start the carousel
+        Ember.$('.carousel').carousel();
+    }
+});
--- app/models/photo.js
+++ app/models/photo.js
@@ -20,6 +20,11 @@ export default DS.Model.extend(ValidationsMixin, {
         return this.get('fileName') ? '//app.wwoof.fr/public/host_photos/' + encodeURIComponent(this.get('fileName')) : '';
     }.property('fileName'),
 
+    // TODO: remove this after bind-attr are gone (=> htmlbars)
+    asBackground: function () {
+        return 'background:url('+ this.get('completeUrl') +') center center; background-size:cover;';
+    }.property('completeUrl'),
+
     // Validations
     validations: {
         caption: {
