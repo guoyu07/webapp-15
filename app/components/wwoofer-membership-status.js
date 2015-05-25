82cf33116002facbc90e688ef345c4f4d79c5a2f
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-05-25 13:45:12 -0700

    Convert wwoof-membership-status to panel

--- app/components/wwoofer-membership-status.js
+++ app/components/wwoofer-membership-status.js
@@ -17,5 +17,22 @@ export default Ember.Component.extend({
             this.set('membershipsService', this.container.lookup('service:user-memberships', { singleton: false }));
             this.get('membershipsService').loadMemberships(userId);
         }
-    }.observes('wwoofer.user.id').on('init')
+    }.observes('wwoofer.user.id').on('init'),
+
+    /**
+     * Returns the CSS class of the panel based on the wwoofer's membership status.
+     */
+    panelClass: function () {
+        var hasWwoofMemberships = this.get('membershipsService.hasWwoofMemberships');
+        var stillGoodInAMonth = this.get('membershipsService.latestWwoofMembership.isStillValidInAMonth');
+
+        var panelClass = 'panel-success';
+        if (!hasWwoofMemberships) {
+            panelClass = 'panel-warning';
+        } else if (!stillGoodInAMonth) {
+            panelClass = 'panel-warning';
+        }
+
+        return panelClass;
+    }.property('membershipsService.hasWwoofMemberships', 'membershipsService.latestWwoofMembership.isStillValidInAMonth')
 });
