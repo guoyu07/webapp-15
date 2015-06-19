4431e9b033ecab98cff2930d96a8b3dc2f9b7832
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-06-18 20:26:03 -0700

    Fix country binding in wwoofers search

--- app/routes/wwoofers/index.js
+++ app/routes/wwoofers/index.js
@@ -5,15 +5,26 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+    countriesService: Ember.inject.service('countries'),
+
     beforeModel: function (transition) {
         this._super(transition);
         this.controllerFor('wwoofers.index').set('isLoading', true);
     },
     model: function (params) {
-        return this.store.find('wwoofer', params);
+        return Ember.RSVP.hash({
+            wwoofers: this.store.find('wwoofer', params),
+            // Pre-load the countries so the queryParams binding
+            // with the select menu work properly
+            countries: this.get('countriesService.countries')
+        });
     },
     afterModel: function() {
         this.controllerFor('wwoofers.index').set('isLoading', false);
+    },
+    setupController(controller, results) {
+        controller.set('model', results.wwoofers);
     },
     actions: {
         searchWwoofers: function () {
