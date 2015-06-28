e2bbb704d2eef5b96b35e3fa279eac9f828629dc
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-06-28 16:28:59 -0700

    Remove thumbnail CSS classes

--- app/components/image-thumb.js
+++ app/components/image-thumb.js
@@ -10,12 +10,18 @@ export default Ember.Component.extend({
     url: null,
 
     /**
+     * The height of the the thumbnail.
+     */
+    height: 200,
+
+    /**
      * Returns the complete style tag based on the url.
      */
-    style: Ember.computed('url', function () {
+    style: Ember.computed('url', 'height', function () {
         var url = this.get('url');
+        var height = this.get('height');
         if (!Ember.isEmpty(url)) {
-            var style = `background:url(${url}) center center; background-size:cover`;
+            var style = `background:url(${url}) center center; background-size:cover; height: ${height}px;`;
             return style.htmlSafe();
         }
     })
