c01e23e64b2eef1b63d4b0c2c62b7238c3bfdfab
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-07-19 11:35:24 -0700

    Only show link to purchase new membership if the host was approved

--- app/controllers/user/memberships.js
+++ app/controllers/user/memberships.js
@@ -6,5 +6,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
     // Local user memberships service (not singleton)
-    localUserMembershipsService: null
+    localUserMembershipsService: null,
+
+    // Only show the link to get a first membership if the host was approved
+    showHostMembershipLink: Ember.computed.and('model.host.id', 'model.host.isApproved')
 });
