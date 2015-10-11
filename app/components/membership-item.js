b6da358a3620b6cc275dc10ea18608522bc7c8d9
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-10-11 16:54:21 -0700

    Allow admins to mark booklet as sent (Fixes #122)

--- app/components/membership-item.js
+++ app/components/membership-item.js
@@ -22,5 +22,24 @@ export default Ember.Component.extend({
         var checked = this.get('checked');
         var membership = this.get('membership');
         this.sendAction('itemToggled', membership, checked);
-    })
+    }),
+
+    expireAtClass: Ember.computed('membership.isExpired', 'membership.isStillValidInAMonth', function () {
+        var cssClass = 'text-success';
+        var isExpired = this.get('membership.isExpired');
+        var isStillValidInAMonth = this.get('membership.isStillValidInAMonth');
+        if (isExpired) {
+            cssClass = 'text-danger';
+        } else if (!isStillValidInAMonth) {
+            cssClass = 'text-warning';
+        }
+        return cssClass;
+    }),
+
+    actions: {
+        markBookletAsSent(membership) {
+            membership.set('bookletSentAt', new Date());
+            membership.save();
+        }
+    }
 });
