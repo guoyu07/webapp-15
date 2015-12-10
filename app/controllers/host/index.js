44ab982d4c12498bc31fc4c12ef1d3c60e7df100
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2015-12-09 22:47:49 -0800

    Add edit profile button on host index page

--- app/controllers/host/index.js
+++ app/controllers/host/index.js
@@ -1,17 +1,28 @@
-/**
- * Ember controller to display a single host.
- */
 import Ember from 'ember';
+
+const { computed } = Ember;
 
 export default Ember.Controller.extend({
   /**
    * Indicates whether the host contact info can be displayed to the current user.
    */
-  canSeeContactInfo: Ember.computed.readOnly('sessionUser.user.hasNonExpiredMembership'),
+  canSeeContactInfo: computed.readOnly('sessionUser.user.hasNonExpiredMembership'),
 
   /**
    * Indicates whether the notes about the host should be displayed.
    * Only admin can see host notes.
    */
-  showNote: Ember.computed.and('model.note', 'sessionUser.user.isAdmin')
+  showNote: computed.and('model.note', 'sessionUser.user.isAdmin'),
+
+  /**
+   * Indicates whether the authenticated user owns the current host profile.
+   */
+  isCurrentUserProfile: computed('sessionUser.user.id', 'model.user.id', function () {
+    return this.get('sessionUser.user.id') === this.get('model.user.id');
+  }),
+
+  /**
+   * Indicates whether the edit profile buttons should be displayed.
+   */
+  showEditProfileButton: computed.or('sessionUser.user.isAdmin', 'isCurrentUserProfile')
 });
