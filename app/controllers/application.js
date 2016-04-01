3b1d5ffdf6f8926966f3b70a1e8eb6210b369e91
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2016-04-01 00:11:51 -0700

    Remove the latest view

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -95,6 +95,22 @@ export default Ember.Controller.extend(ValidationsMixin, {
 
     toggleImpersonationModal() {
       this.toggleProperty('showImpersonationModal');
+    },
+
+    /**
+     * Collapses the navigation menu on mobile/tablet.
+     */
+    navBarClicked(target) {
+      // Do not collapse on desktop (i.e. if the button navbar toggle is hidden)
+      if (Ember.$('button.navbar-toggle').is(':hidden')) {
+        return;
+      }
+      // Do not collapse if target is a sub menu
+      if (Ember.$(target).hasClass('dropdown-toggle')) {
+        return;
+      }
+
+      Ember.$('.navbar-collapse').collapse('hide');
     }
   },
 
