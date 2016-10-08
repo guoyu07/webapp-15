6113bfb4593bff209cca6b86bb1cc261a03fd4d9
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2016-10-08 14:10:45 -0700

    Restrict review edit and delete buttons to authors and admins

--- app/models/review.js
+++ app/models/review.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 const { computed } = Ember;
+const { service } = Ember.inject;
 
 export default DS.Model.extend({
+  sessionUser: service('sessionUser'),
+
   text: DS.attr('string'),
   recipient: DS.attr('string'),
 
@@ -17,8 +20,22 @@ export default DS.Model.extend({
 
   isHostReview: computed.equal('recipient', 'host'),
   isWwooferReview: computed.equal('recipient', 'wwoofer'),
-  
+
   reviewer: computed('recipient', 'host.user', 'wwoofer.user', function() {
     return this.get('recipient') === 'host' ? this.get('wwoofer.user') : this.get('host.user');
+  }),
+
+  /**
+   * Indicates whether the authenticated user is the reviewer.
+   */
+  isCurrentUserReviewer: computed('sessionUser.user.id', 'reviewer.id', function() {
+    return this.get('sessionUser.user.id') === this.get('reviewer.id');
+  }),
+
+  canDelete: computed.or('sessionUser.user.isAdmin', 'isCurrentUserReviewer'),
+
+  canEdit: computed('sessionUser.user.isAdmin', 'isCurrentUserReviewer', 'approvedAt', function () {
+    let authorCanEdit = this.get('isCurrentUserReviewer') && Ember.isEmpty(this.get('approvedAt'));
+    return this.get('sessionUser.user.isAdmin') || authorCanEdit;
   })
 });
