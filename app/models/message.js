ba46acf3bc2ceb2a96b99529693bca7938b6d54e
Sofianne Battikh <sofianne8@gmail.com>
2017-04-08 19:06:43 -0700

    Show messages in panels

--- app/models/message.js
+++ app/models/message.js
@@ -1,6 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const { service } = Ember.inject;
+const { computed } = Ember;
 
 export default DS.Model.extend({
+  sessionUser: service('sessionUser'),
 
   // Attributes
   createdAt: DS.attr('date'),
@@ -10,4 +15,11 @@ export default DS.Model.extend({
   // Relationships
   conversation: DS.belongsTo('conversation', { async: false }),
   author: DS.belongsTo('user', { async: true }),
+
+  /**
+   * Indicates whether the authenticated user is the author of the review.
+   */
+  isCurrentUserAuthor: computed('sessionUser.user.id', 'author.id', function() {
+    return this.get('sessionUser.user.id') === this.get('author.id');
+  }),
 });
