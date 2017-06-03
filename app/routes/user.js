fe30ff9c69722062b9de58ae8a48562ded58de09
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2017-06-03 10:25:00 -0700

    Remove user favorites page

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,15 +4,5 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   titleToken(model) {
     return model.get('fullName');
-  },
-
-  actions: {
-    addFavorite(host) {
-      this.send('addUserFavorite', host, this.controller.get('model'));
-    },
-
-    removeFavorite(host) {
-      this.send('removeUserFavorite', host, this.controller.get('model'));
-    }
   }
 });
