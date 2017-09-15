60ab4454e3562c74cb44f26f3d7ed9f6c0b66591
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2017-09-14 23:12:33 -0700

    Minor fixes

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -7,10 +7,13 @@ export default Ember.Controller.extend({
   user: null,
 
   isCurrentUserProfile: computed('sessionUser.user.id', 'user.id', function() {
-    return this.get('sessionUser.user.id') === this.get('model.user.id');
+    return this.get('sessionUser.user.id') === this.get('user.id');
   }),
 
   showNote: computed.and('user.note', 'sessionUser.user.isAdmin'),
 
-  showEditProfileButton: computed.or('sessionUser.user.isAdmin', 'isCurrentUserProfile')
+  showEditProfileButton: computed.or('sessionUser.user.isAdmin', 'isCurrentUserProfile'),
+
+  hasPhone: computed.notEmpty('user.phone'),
+  showPhone: computed.and('hasPhone', 'sessionUser.user.hasActiveMembership')
 });
