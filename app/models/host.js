d4d599b4663160e3c801bdcdb5d76237eca0141e
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2017-11-04 19:24:08 -0700

    Show number of reviews for each host

--- app/models/host.js
+++ app/models/host.js
@@ -50,6 +50,11 @@ export default DS.Model.extend({
   address: DS.belongsTo('address', { async: false }),
   reviews: DS.hasMany('review', { async: true }),
 
+  // Number of reviews - use this instead of reviews.length to prevent Ember data from fetching the reviews
+  reviewCount: computed('reviews', function() {
+    return this.hasMany('reviews').ids().length;
+  }),
+
   // Reviews sorted by creation date (most recent first)
   createdAtSortingDesc: ['createdAt:desc'],
   sortedReviews: computed.sort('reviews', 'createdAtSortingDesc'),
